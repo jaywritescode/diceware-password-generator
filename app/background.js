164620406859ead6ea4629d50546db4627e4d700
Jay Harris <jaywritescode@users.noreply.github.com>
2019-06-06 09:34:04 -0400

    use async function instead of explicit promises

--- app/background.js
+++ app/background.js
@@ -5,16 +5,14 @@ chrome.runtime.onInstalled.addListener(() => getWordList(`data/${WORD_LIST_FILEN
 
 const WORD_LIST_FILENAMES = ['eff_large_wordlist.txt', 'eff_short_wordlist_1.txt'];
 
-const getWordList = (url) => {
-  fetch(url)
-    .then(response => response.text()
-      .then(text => {
-        const words = _(text).split("\n").map(line => line.split(' ')).fromPairs().value();
-        chrome.storage.local.set({
-          [LOCAL_STORAGE_KEY]: {
-            large_wordlist: words,
-          },
-        }, () => chrome.storage.local.get(LOCAL_STORAGE_KEY, (x) => console.log(x)));
-      }))
-    .catch(console.log)
+ const getWordList = async (url) => {
+  const response = await fetch(url);
+  const text = await response.text();
+
+  const words = _(text).split("\n").map(line => line.split(' ')).fromPairs().value();
+  chrome.storage.local.set({
+    [LOCAL_STORAGE_KEY]: {
+      large_wordlist: words
+    }
+  }, () => chrome.storage.local.get(LOCAL_STORAGE_KEY, (x) => console.log(x)));
 }
