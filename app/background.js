ac2fb126f00b6535e4d94f2aac184eb9fe47f17d
Jay Harris <jaywritescode@users.noreply.github.com>
2019-06-06 12:37:04 -0400

    include both word lists

--- app/background.js
+++ app/background.js
@@ -1,18 +1,42 @@
 import _ from 'lodash';
 import { LOCAL_STORAGE_KEY } from './shared/constants';
 
-chrome.runtime.onInstalled.addListener(() => getWordList(`data/${WORD_LIST_FILENAMES[0]}`))
-
 const WORD_LIST_FILENAMES = ['eff_large_wordlist.txt', 'eff_short_wordlist_1.txt'];
 
- const getWordList = async (url) => {
-  const response = await fetch(url);
-  const text = await response.text();
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    console.log('Storage key "%s" in namespace "%s" changed. ' +
+                'Old value was "%O", new value is "%O".',
+                key,
+                namespace,
+                storageChange.oldValue,
+                storageChange.newValue);
+  }
+});
 
-  const words = _(text).split("\n").map(line => line.split(' ')).fromPairs().value();
-  chrome.storage.local.set({
-    [LOCAL_STORAGE_KEY]: {
-      large_wordlist: words
-    }
-  }, () => chrome.storage.local.get(LOCAL_STORAGE_KEY, (x) => console.log(x)));
-}
+const handleInstalled = async () => {
+  
+  const installWordList = async (filename, path = 'data') => {
+    const words = await getWordList(`${path}/${filename}`);
+    return {
+      [filename]: words,
+    };
+  };
+
+  const getWordList = async (file) => {
+    const response = await fetch(file);
+    const text = await response.text();
+
+    return _(text).split("\n").map(line => line.split(' ')).fromPairs().value();
+  };
+
+  Promise.all([
+    installWordList(WORD_LIST_FILENAMES[0]),
+    installWordList(WORD_LIST_FILENAMES[1]),
+  ]).then(entries => chrome.storage.local.set({
+    [LOCAL_STORAGE_KEY]: Object.assign({}, ...entries),
+  }));
+};
+
+chrome.runtime.onInstalled.addListener(handleInstalled);
